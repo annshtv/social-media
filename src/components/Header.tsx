597eb29b4cd2b75d75674b8aaa97f794195a0d11
Anna <akstanna05@gmail.com>
2025-09-08 09:10:56 +0500

    header & sidebar done

---
+++ src/components/Header.tsx
@@ -0,0 +1,87 @@
+import {
+  Flex,
+  Box,
+  Image,
+  Input,
+  HStack,
+  Avatar,
+  Text,
+  IconButton,
+} from "@chakra-ui/react";
+import {
+  SearchIcon,
+  SettingsIcon,
+  BellIcon,
+  InfoOutlineIcon,
+} from "@chakra-ui/icons";
+import img from "../../Rectangle 5.png";
+
+function Header() {
+  return (
+    <Flex
+      bg="#A9DEF9"
+      align="center"
+      justify="space-between"
+      px="20px"
+      py="10px"
+    >
+      <Box>
+        <Image src={img} boxSize="45px" borderRadius="md" />
+      </Box>
+      <Flex
+        align="center"
+        bg="white"
+        borderRadius="12px"
+        px="4"
+        w="500px"
+        h="40px"
+        boxShadow="sm"
+      >
+        <SearchIcon color="gray.400" mr="2" />
+        <Input
+          placeholder="Search for friends here..."
+          border="none"
+          _focus={{ outline: "none" }}
+          fontSize="14px"
+        />
+      </Flex>
+      <HStack spacing="20px">
+        <HStack spacing="3">
+          <Avatar size="md" name="Steve Rogers" src="../../Group 29.png" />
+          <Box lineHeight="short">
+            <Text fontWeight="bold" fontSize="15px">
+              Steve Rogers
+            </Text>
+            <Text fontSize="13px" color="gray.600">
+              @steve_rogers
+            </Text>
+          </Box>
+        </HStack>
+        <HStack spacing="10px">
+          <IconButton
+            aria-label="Custom Icon"
+            icon={<Image src="../../Group 3.png" boxSize="18px" />}
+          />
+
+          <IconButton
+            aria-label="Custom Icon"
+            icon={<Image src="../../Group 4.png" boxSize="18px" />}
+            borderRadius="md"
+            bg="white"
+            size="sm"
+          />
+
+          <IconButton
+            aria-label="Custom Icon"
+            icon={<Image src="../../Group 5.png" boxSize="18px" />}
+            borderRadius="md"
+            bg="white"
+            size="sm"
+          />
+        </HStack>
+      </HStack>
+    </Flex>
+  );
+}
+
+export default Header;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,90 @@
+import {
+  Box,
+  VStack,
+  Avatar,
+  Text,
+  Heading,
+  HStack,
+  IconButton,
+} from "@chakra-ui/react";
+import { SettingsIcon, LockIcon } from "@chakra-ui/icons";
+import {
+  FaUser,
+  FaUserFriends,
+  FaChartBar,
+  FaSignOutAlt,
+} from "react-icons/fa";
+
+function Sidebar() {
+  return (
+    <Box bg="white" w="250px" minH="100vh" p="20px">
+      <VStack spacing="3" mb="8">
+        <Avatar
+          size="xl"
+          name="Steve Rogers"
+          src="../../Rectangle 7.png"
+          borderRadius="20px"
+        />
+        <Text fontWeight="bold" fontSize="18px">
+          Steve Rogers
+        </Text>
+      </VStack>
+      <Box mb="6">
+        <Heading size="sm" color="red.400" mb="3">
+          Explore panel
+        </Heading>
+        <VStack align="stretch" spacing="3">
+          <HStack>
+            <Box bg="#A9DEF9" p="2" borderRadius="md">
+              <FaUser />
+            </Box>
+            <Text>Profile</Text>
+          </HStack>
+          <HStack>
+            <Box bg="#A9DEF9" p="2" borderRadius="md">
+              <FaUserFriends />
+            </Box>
+            <Text>Find friends</Text>
+          </HStack>
+          <HStack>
+            <Box bg="#A9DEF9" p="2" borderRadius="md">
+              <FaChartBar />
+            </Box>
+            <Text>User analytics</Text>
+          </HStack>
+        </VStack>
+      </Box>
+      <Box mb="6">
+        <Heading size="sm" color="red.400" mb="3">
+          Settings
+        </Heading>
+        <VStack align="stretch" spacing="3">
+          <HStack>
+            <Box bg="#A9DEF9" p="2" borderRadius="md">
+              <SettingsIcon />
+            </Box>
+            <Text>Settings</Text>
+          </HStack>
+          <HStack>
+            <Box bg="#A9DEF9" p="2" borderRadius="md">
+              <LockIcon />
+            </Box>
+            <Text>Security data</Text>
+          </HStack>
+        </VStack>
+      </Box>
+      <Box>
+        <HStack>
+          <Box bg="red.400" p="2" borderRadius="md" color="white">
+            <FaSignOutAlt />
+          </Box>
+          <Text color="red.500" fontWeight="bold">
+            Log out
+          </Text>
+        </HStack>
+      </Box>
+    </Box>
+  );
+}
+
+export default Sidebar;
